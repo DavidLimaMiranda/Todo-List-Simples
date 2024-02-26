c99fd983b41aec441346ed871c05b42550426d49
David Lima <davidprofissional@gmail.com>
2024-02-26 14:27:16 -0300

    Maioria das Exceptions gerais e personalizadas feitas, Nova dependencia no pom.xml

--- src/main/java/com/project/todosimples/models/Task.java
+++ src/main/java/com/project/todosimples/models/Task.java
@@ -4,15 +4,15 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Objects;
 
 @Entity
 @Table(name = "task")
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Task {
 
     @Id
@@ -37,25 +37,5 @@ public class Task {
         this.id = id;
         this.users = users;
         this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-
-        if (obj == null || getClass() != obj.getClass()) return false;
-
-        Task task = (Task) obj;
-        return Objects.equals(id, task.id) && Objects.equals(users, task.users) && Objects.equals(description, task.description);
-    }
-
-    @Override
-    public int hashCode() {
-
-        final int prime = 31;
-        int result = 1;
-
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-        return result;
     }
 }
